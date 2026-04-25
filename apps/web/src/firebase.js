53b3d0ad38e7bef7f805fcf6eabb19d8e515b9b3
AniruddhDeepak <aniruddhdeepak2007@gmail.com>
2026-04-25 21:57:49 +0530

    chore: secure firebase config by moving keys to env variables

--- apps/web/src/firebase.js
+++ apps/web/src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCemGFjwQYq3yy8RwIpzLxW8w94pT6OUUc",
-  authDomain: "chainhandler-5b292.firebaseapp.com",
-  projectId: "chainhandler-5b292",
-  storageBucket: "chainhandler-5b292.firebasestorage.app",
-  messagingSenderId: "558261279032",
-  appId: "1:558261279032:web:acd20bf196192d49c689cd",
-  measurementId: "G-0XESK94KRT"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
